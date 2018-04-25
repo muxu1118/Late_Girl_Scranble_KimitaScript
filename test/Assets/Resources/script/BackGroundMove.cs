e38d709e83140110afe752f4a027732a2e43ba1b
muxu1118 <minecraft.nichijo@ezweb.ne.jp>
2018-04-25 18:45:01 +0900

    とりまコミット

---
+++ test/Assets/Resources/script/BackGroundMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMove : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(-0.1f, 0, 0);
+        if (transform.position.x < -10.65f*2)
+        {
+            transform.position = new Vector3(10.65f * 2, -1, 0);
+        }
+    }
+}
---
+++ test/Assets/Resources/script/player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour 
+    {
+
+    public float speed = 0f; //プレイヤーの速さ
+    public Sprite[] walk; //プレイヤーの歩くスプライト配列
+    int animIndex; //歩くアニメーションのインデックス
+    bool walkCheck; //歩いているかのチェック
+    public float flame = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        animIndex = 0;
+        walkCheck = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        flame += 0.1f;
+        if (flame <= 20)
+        {
+            GetComponent<SpriteRenderer>().sprite = walk[0];
+        }else if (flame >= 20||flame <= 40)
+        {
+            GetComponent<SpriteRenderer>().sprite = walk[1];
+        }else if (flame <= 60|| flame >= 40)
+        {
+            GetComponent<SpriteRenderer>().sprite = walk[2];
+        }
+        else
+        {
+            flame = 0;
+        }
+    }
+    void Walk()
+    {
+        
+        //歩くアニメーションの再生
+        animIndex++;
+        if (animIndex >= walk.Length)
+        {
+            animIndex = 0;
+        }
+        GetComponent<SpriteRenderer>().sprite = walk[animIndex];
+
+    }
+    //「コルーチン」で呼び出すメソッド
+    IEnumerator sleep()
+    {
+        yield return new WaitForSeconds(5);  //1秒待つ
+    }
+}
