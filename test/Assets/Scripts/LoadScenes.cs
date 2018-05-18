a7a5e9dc0d24f2d4e5a17de371e131838bf713e2
田村航海 <17c1020017tk@edu.tech.ac.jp>
2018-05-18 09:22:52 +0900

    OP一時保存

---
+++ test/Assets/Scripts/LoadScenes.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class LoadingScenes : MonoBehaviour {
+
+	[SerializeField]
+	private AsyncOperation async;
+	public GameObject LoadingUI;
+	public Slider Slider;
+	public void LoadNextScene(){
+		LoadingUI.SetActive(true);
+		StartCoroutine(LoadScene());
+	}
+
+	IEnumerator LoadScene(){
+		async = SceneManager.LoadSceneAsync("");
+		while(!async.isDone){
+			Slider.value = async.progress;
+			yield return null;
+		}
+	}
+}
