4532b1d118ad9fd58b1fc40d85a9571bd3f88a99
田村航海 <17c1020017tk@edu.tech.ac.jp>
2018-05-23 17:22:18 +0900

    とりまコミット

--- test/Assets/Scripts/LoadingScenes.cs
+++ test/Assets/Scripts/LoadingScenes.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 
 public class LoadingScenes : MonoBehaviour {
-
+	/*
 	[SerializeField]
 	private AsyncOperation async;
 	public GameObject LoadingUI;
@@ -23,4 +23,24 @@ public class LoadingScenes : MonoBehaviour {
 			yield return null;
 		}
 	}
+	*/
+
+	[SerializeField]
+	private float FadeInTime;
+
+	private Image image;
+	private void Start()
+	{
+		image = transform.Find("Panel").GetComponent<Image>();
+		FadeInTime = 1f * FadeInTime / 10f;
+		StartCoroutine("FadeIn");
+
+	}
+	IEnumerator FadeIn(){
+		for (var i = 1f; i >= 0;i-=0.1f){
+			image.color = new Color(0f, 0f, 0f, i);
+			yield return new WaitForSeconds(FadeInTime);
+		}
+	}
+
 }
