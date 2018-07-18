8bc16e6c0e265d182d373d6fa373bde7de56c1e5
KURO <kuro@trainfo.tokyo>
2018-07-18 17:57:51 +0900

    sinario復活

---
+++ LateGirl_Scramble/Assets/Scripts/SinarioManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SinarioManager : MonoBehaviour {
+	[SerializeField]
+	private List<Sprite> SinarioSprite = new List<Sprite>();
+	[SerializeField]
+	int num;
+	[SerializeField]
+	private Image image;
+	[SerializeField]
+	private bool flag = false;
+	// Use this for initialization
+	void Start () {
+		image.sprite = SinarioSprite[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (flag == false)
+		{
+			if (Input.GetMouseButtonDown(0) || Input.GetKey("space"))
+			{
+				num++;
+				if (num < SinarioSprite.Count)
+				{
+
+					image.sprite = SinarioSprite[num];
+					Debug.Log(num);
+
+				}
+				else
+				{
+					flag = true;
+					SceneLoadManager.LoadScene("mainScene");
+				}
+
+			}
+		}
+
+	}
+}
